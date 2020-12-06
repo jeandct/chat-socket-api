262f7fa5c273af6c0170dceda587d35d219d22dd
Jean Ducret <jean.dcrt@gmail.com>
2020-12-06 15:40:18 +0100

    fix connected users

--- app.js
+++ app.js
@@ -24,15 +24,20 @@ let users = [];
 
 io.on('connection', (socket) => {
   socket.emit('connection', `Client connected as id : ${socket.id}`);
-  console.log(io.engine.clientsCount);
+  // console.log(io.engine.clientsCount);
   // socket.emit('FromAPI', 'Hello user ! You are connected as' + socket.id);
   socket.on('user_message', (message) => {
-    console.log(message);
+    // console.log(message);
     io.emit('chat_message', message);
   });
 
   socket.on('user_connected', (user) => {
-    users.push(user);
+    users.push({ id: socket.id, username: user });
+    io.emit('all_users_connected', users);
+  });
+
+  socket.on('disconnect', () => {
+    users = users.filter((user) => user.id != socket.id);
     io.emit('all_users_connected', users);
   });
 });
